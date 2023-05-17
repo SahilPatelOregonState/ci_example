7a9be5b913ccc60b4e6df0507574d5744d99b1a5
SahilPatelOregonState <patelsah@oregonstate.edu>
2023-05-17 19:38:20 -0400

    added task file and unittests to tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,12 @@
 import unittest
+from task import my_func
+
+
+class Testcase(unittest.TestCase):
+    def test1(self):
+        expected = "Hello World!"
+        self.assertEqual(expected, my_func())
+
+
 if __name__ == '__main__':
     unittest.main()
